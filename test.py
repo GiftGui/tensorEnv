20fc3ccdfe2fb0124b1f14e74dc14025af9ec107
GiftGui <417695469@qq.com>
2024-02-03 11:27:32 +0800

    content about linear regression

--- test.py
+++ test.py
@@ -10,6 +10,24 @@ print(data.head())
 
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
-print(data.head())
+predict = "G3"
 
+X = np.array(data.drop([predict], 1))
+Y = np.array(data[predict])
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print(acc)
+
+print('Coefficienct: \n', linear.coef_)
+print('Intercept: \n', linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for i in range(len(predictions)):
+    print(predictions[i], x_test[i], y_test[i])
 
