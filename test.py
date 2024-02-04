8c246c35a835d19ee74728fb5da80b2c011e6abd
GiftGui <417695469@qq.com>
2024-02-04 08:23:04 +0800

    train the model and display it with plot

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 from sklearn.utils import shuffle
 
 data = pd.read_csv("student-mat.csv", sep=";")
@@ -17,11 +20,17 @@ Y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size = 0.1)
 
-linear = linear_model.LinearRegression()
+"""linear = linear_model.LinearRegression()
 
 linear.fit(x_train, y_train)
 acc = linear.score(x_test, y_test)
 print(acc)
+
+with open("studentmodel.pickle", "wb") as f:
+    pickle.dump(linear, f)"""
+
+pickle_in = open("studentmodel.pickle", "rb")
+linear = pickle.load(pickle_in)
 
 print('Coefficienct: \n', linear.coef_)
 print('Intercept: \n', linear.intercept_)
@@ -31,3 +40,9 @@ predictions = linear.predict(x_test)
 for i in range(len(predictions)):
     print(predictions[i], x_test[i], y_test[i])
 
+p = 'G1'
+style.use("ggplot")
+pyplot.scatter(data[p], data["G3"])
+pyplot.xlabel(p)
+pyplot.ylabel("Final Grade")
+pyplot.show()
